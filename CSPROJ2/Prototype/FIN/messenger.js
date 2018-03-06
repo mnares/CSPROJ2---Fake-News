bd18b27756717ab3c34fd7f436a7937b8ca58a1a
acdeleon <deleonaleoralphcastro@gmail.com>
2018-03-06 08:05:30 +0800

    Added working prototype

---
+++ CSPROJ2/Prototype/FIN/messenger.js
@@ -0,0 +1,34 @@
+(function() {
+	"use strict";
+	var btn1 = new Object();
+	var btn2 = new Object(); 
+	
+	btn1.title = chrome.i18n.getMessage("notAccurate");
+	btn2.title = chrome.i18n.getMessage("Accurate");
+	var opt = {
+		type: "basic",
+		title: "Fake News",
+		message: "",
+		iconUrl: "icons/logo128_fake.png",
+		buttons: [btn1, btn2]
+	};
+	
+	function btnClick(notid, btninx) {
+		if (btninx === 0) {
+			window.open("https://github.com/Fdebijl/FakeNewsBlocker/issues");
+			chrome.notifications.clear(notid);
+		} else {
+			chrome.notifications.clear(notid);
+		}
+	}
+
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		if (request.t === "notu") {
+			opt.message = chrome.i18n.getMessage("Attent", request.l)
+			chrome.notifications.create('thisisfakenews', opt, null)
+			chrome.notifications.onButtonClicked.addListener(btnClick);
+			sendResponse();
+		}
+	});
+	
+})();
